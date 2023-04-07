e5f465dba3ec9b960e84b733be001d7d184efc76
abalachandar <123904144+abalachandar@users.noreply.github.com>
2023-04-07 03:28:53 -0400

    Update StatementTest.java

--- StatementTest.java
+++ StatementTest.java
@@ -11,7 +11,7 @@ import components.utilities.Tokenizer;
 /**
  * JUnit test fixture for {@code Statement}'s constructor and kernel methods.
  *
- * @author Put your name here
+ * @author Adithya and Majed
  *
  */
 public abstract class StatementTest {
