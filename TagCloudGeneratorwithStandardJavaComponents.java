c66696d8bac0c01666197ab0d586b9199340d753
Maj0071 <91580746+Maj0071@users.noreply.github.com>
2023-04-19 23:46:10 -0400

    Add files via upload

---
+++ TagCloudGeneratorwithStandardJavaComponents.java
@@ -0,0 +1,316 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * Generates tag cloud from a given input text
+ *
+ * @author Adithya Balachandar and Majed Ahmad
+ *
+ */
+public final class TagCloudGeneratorwithStandardJavaComponents {
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private TagCloudGeneratorwithStandardJavaComponents() {
+
+    }
+
+    /**
+     *
+     * Creates a alphabetical comparator.
+     *
+     */
+    private static abstract class Count
+            implements Comparator<Map.Entry<String, Integer>>, Serializable,
+            Entry<String, Integer> {
+        @Override
+        public int compare(Map.Entry<String, Integer> pair1,
+                Map.Entry<String, Integer> pair2) {
+            return pair2.getValue().compareTo(pair1.getValue());
+        }
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    /**
+     *
+     * Creates a count comparator.
+     *
+     */
+    private static class Alphabetize
+            implements Comparator<Map.Entry<String, Integer>>, Serializable {
+        @Override
+        public int compare(Map.Entry<String, Integer> pair1,
+                Map.Entry<String, Integer> pair2) {
+            return pair2.getKey().compareTo(pair1.getKey());
+        }
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    /**
+     * Creates the table of words and its occurences on an HTML page.
+     *
+     * @param outputFile
+     *            the output file that the table is put on and shown to the
+     *            user.
+     * @param inputTitle
+     *            the title of the input file
+     * @param numbers
+     *            number of words in the Tag generator
+     * @param countTable
+     *            the {@code Map} that's being printed out in a table
+     * @param sortTable
+     *            the individual words contained in table being sorted
+     *
+     */
+    private static void generateTable(String outputFile, String inputTitle,
+            int numbers, Map<String, Integer> countTable, List sort) {
+
+//        int max = 48;
+//        int min = 11;
+
+        PrintWriter out;
+        //Creates WebPage
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Top " + numbers + " Words in " + inputTitle
+                + "</title>");
+        out.println("<link href=\"http://web.cse.ohio-state.edu/software/2231"
+                + "/web-sw2/assignments/projects/tag-cloud-generation/data"
+                + "/tagcloud.css\" rel=\"stylesheet\" type=\"text/css\">");
+        out.println(
+                "<link href=\"doc/tagcloud.css\" rel=\"stylesheet\" type=\"text/css\">");
+        out.println("</head>");
+
+        //makes header
+        out.println("<body>");
+        out.println("<h2>Top " + numbers + " Words in " + inputTitle + "</h2>");
+        out.println("<hr>");
+        out.println("<div class=\"cdiv\">");
+        out.println("<p class=\"cbox\">");
+
+        //HTML for sorted words
+        int position = sort.size() - 1;
+        while (sort.size() > 0) {
+            Entry<String, Integer> word = sort.remove(position);
+            out.println("<span style=\"cursor:default\" class=\"" + "f"
+                    + word.getValue().toString() + "\" title=\"count: "
+                    + word.getValue() + "\">" + word.getKey() + "</span>");
+
+//          while (sortTable.size() > 0) {
+//              Pair<String, Integer> sort = sortTable.removeFirst();
+//              out.println("<span style=\"cursor:default\" class=\""
+//                      + (((double) sort.value() - Integer.MAX_VALUE)
+//                              / (Integer.MIN_VALUE - Integer.MAX_VALUE))
+//                      + sort.value().toString() + "\" title=\"count: "
+//                      + sort.value() + "\">" + sort.key() + "</span>");
+//      }
+        }
+        //closing html out
+        out.println("</p>");
+        out.println("</div>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+
+    /**
+     * Counts the number of occurences of each word in the inputted text.
+     *
+     * @param input
+     *            the text file inputted by the user
+     * @param table
+     *            the {@code Map} that pairs the word with its number of
+     *            occurences
+     */
+    public static void wordCounter(BufferedReader input,
+            Map<String, Integer> table) {
+
+        //initializes seperators that may interfere with word count putting them
+        //into a set
+        final String seperators = " /n,.!?-@#$%^&*()_:'[]";
+        Set<Character> seperatorSet;
+        for (int i = 0; i < seperators.length(); i++) {
+            seperatorSet.add(seperators.charAt(i));
+        }
+
+        //iterates through each individual line of the inputted text
+        while (!(input == null)) {
+
+            String line = input.nextLine();
+
+            int i = 0;
+            while (i < line.length()) {
+                //initializes word to either a string or seperator
+                String word = nextWordOrSeparator(line, i, seperatorSet);
+                //if word is already a key, its value increases, if not a key
+                //is created for it
+                if (table.containsKey(word)) {
+                    table.replace(word, table.get(word) + 1);
+                } else if (!seperatorSet.contains(word.charAt(0))) {
+                    table.put(word, 1);
+                }
+                //moves position
+                i += word.length();
+            }
+
+        }
+    }
+
+    /**
+     * Returns the first "word" (maximal length string of characters not in
+     * {@code separators}) or "separator string" (maximal length string of
+     * characters in {@code separators}) in the given {@code text} starting at
+     * the given {@code position}.
+     *
+     * @param text
+     *            the {@code String} from which to get the word or separator
+     *            string
+     * @param position
+     *            the starting index
+     * @param separators
+     *            the {@code Set} of separator characters
+     * @return the first word or separator string found in {@code text} starting
+     *         at index {@code position}
+     * @requires 0 <= position < |text|
+     * @ensures <pre>
+     * nextWordOrSeparator =
+     *   text[position, position + |nextWordOrSeparator|)  and
+     * if entries(text[position, position + 1)) intersection separators = {}
+     * then
+     *   entries(nextWordOrSeparator) intersection separators = {}  and
+     *   (position + |nextWordOrSeparator| = |text|  or
+     *    entries(text[position, position + |nextWordOrSeparator| + 1))
+     *      intersection separators /= {})
+     * else
+     *   entries(nextWordOrSeparator) is subset of separators  and
+     *   (position + |nextWordOrSeparator| = |text|  or
+     *    entries(text[position, position + |nextWordOrSeparator| + 1))
+     *      is not subset of separators)
+     * </pre>
+     */
+    private static String nextWordOrSeparator(String text, int position,
+            Set<Character> separators) {
+        assert text != null : "Violation of: text is not null";
+        assert separators != null : "Violation of: separators is not null";
+        assert 0 <= position : "Violation of: 0 <= position";
+        assert position < text.length() : "Violation of: position < |text|";
+
+        String result = "";
+        char character = text.charAt(position);
+        int i = position;
+        if (separators.contains(character)) {
+            while (i < text.length() && separators.contains(text.charAt(i))) {
+                character = text.charAt(i);
+                result += character;
+                i++;
+            }
+        } else {
+            while (i < text.length() && !separators.contains(text.charAt(i))) {
+                character = text.charAt(i);
+                result += character;
+                i++;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * organizes a given map in alphabetical order.
+     *
+     * @param words
+     *            words in inputted text along with each number of occurences.
+     * @param numWrds
+     * @return organized {@code Queue} in alphabetical order.
+     */
+    private static List sort(
+            Map<String, Integer> words, int numWrds) {
+
+       Map.Entry<String, Integer> countComp; //= new Count();
+       Map.Entry<String, Integer> sortNum = //new SortingMachine1L<>(
+   //                countComp);
+
+        //inputs each word into queue
+        for (Map.Entry<String, Integer> temp : words) {
+            sortNum.add(temp);
+        }
+        Comparator<Map.Entry<String, Integer>> wordComp = new Alphabetize();
+        List sortWords;
+
+        int sz = sortNum.size();
+        if (numWrds > sz) {
+            for (int i = 0; i < sz; i++) {
+
+                Map.Entry<String, Integer> temp = sortNum.remove();
+                sortWords.add(temp);
+            }
+        } else {
+            for (int i = 0; i < numWrds; i++) {
+                Map.Entry<String, Integer> temp = sortNum.remove();
+
+                sortWords.add(temp);
+            }
+        }
+        return sortWords;
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments; unused here
+     */
+    public static void main(String[] args) {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader inFile;
+        PrintWriter out;
+
+        //asks for user input file and output file name
+        args[0] = ("Input file name");
+        String input = "";
+        try {
+            input = in.readLine();
+        }catch(IOException e) {
+
+        }
+        args[1] = ("Enter the name of an output file");
+        String output = "";
+        try {
+            output = in.readLine();
+        }catch(IOException e) {
+
+        }
+        args[2] = ("Enter a number of words to be included");
+        int wordsNum = 0;
+        try {
+            wordsNum = Integer.parseInt(in.readLine());
+        }catch(IOException e){
+
+        }
+
+        //creates a map for word and its number of occurences
+        Map<String, Integer> table;
+        BufferedReader inputFile;
+        wordCounter(inputFile, table);
+        sort(table, wordsNum);
+        //calls upon method to get final result
+        PrintWriter outputFile;
+
+        generateTable(output, input, wordsNum, table,);
+
+        //closes streams
+        inputFile.close();
+        outputFile.close();
+    }
+//data/importance.txt
+}
